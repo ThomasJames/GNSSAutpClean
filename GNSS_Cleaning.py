4d3951ee4285392be1e961dc662582bc16196fcf
Thomas James <42183357+ThomasJames@users.noreply.github.com>
2020-04-16 22:43:28 +0100

    Clean function completed

--- GNSS_Cleaning.py
+++ GNSS_Cleaning.py
@@ -3,7 +3,6 @@ import matplotlib.pyplot as plt
 from scipy import stats
 from statistics import stdev
 
-
 """
 For some basic example data that reflects the nature of GNSS receiver.
 The X, Y and Z coordinates are derived using a random normal function on python. 
@@ -14,8 +13,10 @@ X_raw = [None] * 500
 Y_raw = [None] * 500
 Z_raw = [None] * 500
 
+
 def mean(data):
-    return sum(data)/len(data)
+    return sum(data) / len(data)
+
 
 for i in range(500):
     X_raw[i] = np.random.normal(10)
@@ -39,48 +40,57 @@ plt.show()
 """
 Request confidence interval from user.
 """
-def clean_data(X_raw, Y_raw, Z_raw):
 
-    # CI = int(input("Confidence level: "))
 
-    # CI = (CI / 100)/2
-    # print(CI)
-    # Define threshold
+def clean(x_raw, y_raw, z_raw):
+    z_x = list(stats.zscore(x_raw))
+    z_y = list(stats.zscore(y_raw))
+    z_z = list(stats.zscore(z_raw))
 
-    X_z = stats.zscore(np.array(X_raw))
-    Y_z = stats.zscore(np.array(Y_raw))
-    Z_z = stats.zscore(np.array(Z_raw))
+    x = []
+    y = []
+    z = []
 
-    print(X_z)
-
-    X_clean = [None] * len(X_raw)
-    Y_clean = [None] * len(Y_raw)
-    Z_clean = [None] * len(Z_raw)
-
-    for i in range(len(X_raw)):
-        if X_z[i] > -1:
-            X_clean[i] = X_raw[i]
-            Y_clean[i] = Y_raw[i]
-            Z_clean[i] = Z_raw[i]
+    for i in range(len(z_x)):
+        if 1 > z_x[i] > -1:
+            if 1 > z_y[i] > -1:
+                if 1 > z_z[i] > -1:
+                    x.append(x_raw[i])
+                    y.append(y_raw[i])
+                    z.append(z_raw[i])
         else:
-            X_clean[i] = None
-            Y_clean[i] = None
-            Z_clean[i] = None
+            x.append(None)
+            y.append(None)
+            z.append(None)
 
-    for i in range(len(Z_clean)):
-        print("X:" + str(X_clean[i]), "Y:" + str(Y_clean), "Z" + str(Z_clean))
-          
-print(clean_data(X_raw, Y_raw, Z_raw))
+    for i in x, y, z:
+        if i == None:
+            x.remove(i)
+            y.remove(i)
+            z.remove(i)
 
+    print(len(x_raw) - len(x), " values removed")
 
-
-
-
-
-
+    return x, y, z
 
 
+x, y, z = (clean(X_raw, Y_raw, Z_raw))
 
+print(x)
+
+# # Standard deviation = SD of X x Y x Z (Plotting)
+# x, y, z = stdev(x), stdev(y), stdev(z)
+# sd = z * y * z
+#
+# fig = plt.figure()
+# ax = fig.add_subplot(111, projection='3d')
+# ax.grid(False)
+# ax.scatter(x, y, z, color="skyblue")
+# ax.scatter(np.average(x), np.average(y), np.average(z), color="salmon", s=sd)
+# ax.set_xlabel('X')
+# ax.set_ylabel('Y')
+# ax.set_zlabel('Z')
+# plt.show()
 
 # fig = plt.figure()
 # ax = fig.add_subplot(111, projection='3d')
@@ -93,13 +103,10 @@ print(clean_data(X_raw, Y_raw, Z_raw))
 # plt.show()
 
 
-
-
-
-    # for i in range(len(X_raw)):
-    #     if X_z[i] > 3:
-    #         X_clean.append(None)
-    #         Y_clean.append(None)
-    #         Z_clean.append(None)
-    #     else:
-    #         X_clean.append(X_raw[i])
+# for i in range(len(X_raw)):
+#     if X_z[i] > 3:
+#         X_clean.append(None)
+#         Y_clean.append(None)
+#         Z_clean.append(None)
+#     else:
+#         X_clean.append(X_raw[i])
